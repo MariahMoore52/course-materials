4b1085c1bff4e51915b47bb624371adfbc37b56b
mmoore52 <mmoore52@uwyo.edu>
2022-03-08 12:52:17 -0500

    Added Course

--- materials/lab/4/main/main.go
+++ materials/lab/4/main/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	wyoassign.InitAssignments()
+	wyoassign.InitCourses()
 	log.Println("starting API server")
 	//create a new router
 	router := mux.NewRouter()
@@ -21,6 +22,12 @@ func main() {
 	router.HandleFunc("/assignment/{id}", wyoassign.DeleteAssignment).Methods("DELETE")		
 	router.HandleFunc("/assignment", wyoassign.CreateAssignment).Methods("POST")	
 	router.HandleFunc("/assignments/{id}", wyoassign.UpdateAssignment).Methods("PUT")
+	router.HandleFunc("/api-status", wyoassign.APISTATUS).Methods("GET")
+	router.HandleFunc("/courses", wyoassign.GetCourses).Methods("GET")
+	router.HandleFunc("/course/{id}", wyoassign.GetCourse).Methods("GET")
+	router.HandleFunc("/course/{id}", wyoassign.DeleteCourse).Methods("DELETE")		
+	router.HandleFunc("/course", wyoassign.CreateCourse).Methods("POST")	
+	router.HandleFunc("/courses/{id}", wyoassign.UpdateCourse).Methods("PUT")
 
 	http.Handle("/", router)
 
--- materials/lab/4/wyoassign/wyoassign.go
+++ materials/lab/4/wyoassign/wyoassign.go
@@ -13,6 +13,9 @@ import (
 type Response struct{
 	Assignments []Assignment `json:"assignments"`
 }
+type Responses struct{
+	Courses []Course `json:"courses"`
+}
 
 type Assignment struct {
 	Id string `json:"id"`
@@ -21,9 +24,16 @@ type Assignment struct {
 	Points int `json:"points"`
 	//DueDate string `json:"duedate"`
 }
+type Course struct{
+	Id string `json:"id"`
+	Course string `json:"course` 
+	CourseNumber int `json:"coursenumber"`
+}
 
 var Assignments []Assignment
 const Valkey string = "FooKey"
+
+var Courses []Course
 
 func InitAssignments(){
 	var assignmnet Assignment
@@ -33,6 +43,14 @@ func InitAssignments(){
 	assignmnet.Points = 20
 	//assignmnet.DueDate = "3/11/2022";
 	Assignments = append(Assignments, assignmnet)
+}
+func InitCourses(){
+	var cours Course
+	cours.Id = "Tuesdays"
+	cours.Course = "Linux"
+	cours.CourseNumber = 3750
+	//assignmnet.DueDate = "3/11/2022";
+	Courses = append(Courses, cours)
 }
 
 func APISTATUS(w http.ResponseWriter, r *http.Request) {
@@ -47,6 +65,24 @@ func GetAssignments(w http.ResponseWriter, r *http.Request) {
 	var response Response
 
 	response.Assignments = Assignments
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	
+	jsonResponse, err := json.Marshal(response)
+
+	if err != nil {
+		return
+	}
+
+	//TODO 
+	w.Write(jsonResponse)
+}
+func GetCourses(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Entering %s end point", r.URL.Path)
+	var response Responses
+
+	response.Courses = Courses
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -80,6 +116,25 @@ func GetAssignment(w http.ResponseWriter, r *http.Request) {
 	//w.Write(jsonResponse)
 }
 
+func GetCourse(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Entering %s end point", r.URL.Path)
+	w.Header().Set("Content-Type", "application/json")
+	
+	params := mux.Vars(r)
+
+	for _, course := range Courses {
+		if course.Id == params["id"]{
+			json.NewEncoder(w).Encode(course)
+			w.WriteHeader(http.StatusOK)
+			break
+		}else{
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+	//TODO : Provide a response if there is no such assignment
+	//w.Write(jsonResponse)
+}
+
 func DeleteAssignment(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Entering %s DELETE end point", r.URL.Path)
 	w.Header().Set("Content-Type", "application/txt")
@@ -92,6 +147,29 @@ func DeleteAssignment(w http.ResponseWriter, r *http.Request) {
 	for index, assignment := range Assignments {
 			if assignment.Id == params["id"]{
 				Assignments = append(Assignments[:index], Assignments[index+1:]...)
+				response["status"] = "Success"
+				break
+			}
+	}
+		
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
+}
+func DeleteCourse(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Entering %s DELETE end point", r.URL.Path)
+	w.Header().Set("Content-Type", "application/txt")
+	w.WriteHeader(http.StatusOK)
+	params := mux.Vars(r)
+	
+	response := make(map[string]string)
+
+	response["status"] = "No Such ID to Delete"
+	for index, course := range Courses {
+			if course.Id == params["id"]{
+				Courses = append(Courses[:index], Courses[index+1:]...)
 				response["status"] = "Success"
 				break
 			}
@@ -135,7 +213,35 @@ func UpdateAssignment(w http.ResponseWriter, r *http.Request) {
 	}
 	//w.Write(jsonResponse)
 }
+func UpdateCourse(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Entering %s end point", r.URL.Path)
+	w.Header().Set("Content-Type", "application/json")
+	
+	var response Responses
+	response.Courses = Courses
 
+	params := mux.Vars(r)
+
+	w.Header().Set("Content-Type", "application/json")
+	var cours Course
+	r.ParseForm()
+	for index, course := range Courses {
+		if course.Id == params["id"]{
+			Courses = append(Courses[:index], Courses[index+1:]...)
+			cours.Id = r.FormValue("id")
+			cours.Course = r.FormValue("course")
+			cours.CourseNumber, _ = strconv.Atoi(r.FormValue("coursenumber"))
+			Courses = append(Courses, cours)
+			w.WriteHeader(http.StatusCreated)
+			break
+		}else{
+			w.WriteHeader(http.StatusNotFound)
+		}
+		
+		
+	}
+	//w.Write(jsonResponse)
+}
 func CreateAssignment(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var assignmnet Assignment
@@ -151,7 +257,27 @@ func CreateAssignment(w http.ResponseWriter, r *http.Request) {
 		//assignmnet.DueDate, _ = r.FormValue("duedate")
 		Assignments = append(Assignments, assignmnet)
 		w.WriteHeader(http.StatusCreated)
+	}else{
+		w.WriteHeader(http.StatusNotFound)
 	}
-	w.WriteHeader(http.StatusNotFound)
+
+}
+
+func CreateCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var cours Course
+	r.ParseForm()
+	// Possible TODO: Better Error Checking!
+	// Possible TODO: Better Logging
+	if(r.FormValue("id") != ""){
+		
+		cours.Id = r.FormValue("id")
+		cours.Course = r.FormValue("course")
+		cours.CourseNumber, _ = strconv.Atoi(r.FormValue("coursenumber"))
+		Courses = append(Courses, cours)
+		w.WriteHeader(http.StatusCreated)
+	}else{
+		w.WriteHeader(http.StatusNotFound)
+	}
 
 }
